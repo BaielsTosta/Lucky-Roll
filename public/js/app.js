1ef957fb493cc298dc493ae75aedec48e5255456
BaielsTosta <nunestosta@gmail.com>
2025-10-30 16:34:20 -0300

    Função de Adicionar dado

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
-function addDices() {}
+function addDices() {
+  // Selecionando o container que aglomera tudo
+  let containerPrincipal = document.querySelector("#dice-selection");
 
-function rollDices() {}
+  // Selecionando a div que queremos copiar
+  let personalizacaoDados = document
+    .querySelector(".dice-information")
+    .cloneNode(true);
+
+  // Copiando a div para o final do container ao clicar no +
+  containerPrincipal.appendChild(personalizacaoDados);
+}
+
+function rollDices() {
+  let contagem = document.querySelectorAll(".dice-information").length;
+  window.alert(contagem);
+}
