0b125c17bcece161c665609c01441e82fec14e58
BaielsTosta <nunestosta@gmail.com>
2025-11-02 03:50:50 -0300

    Função Roll Dices Funcional. Rolagens aparecendo separadamente

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,54 @@
-function addDices() {
-  // Selecionando o container que aglomera tudo
-  let containerPrincipal = document.querySelector("#dice-selection");
+// Função para adicionar Dados
+function addDices() {}
 
-  // Selecionando a div que queremos copiar
-  let personalizacaoDados = document
-    .querySelector(".dice-information")
-    .cloneNode(true);
-
-  // Copiando a div para o final do container ao clicar no +
-  containerPrincipal.appendChild(personalizacaoDados);
-}
-
+// Função para rolar os Dados
 function rollDices() {
-  let quantidadeDeDados = parseInt(
-    document.querySelector(".dice-quantity-input").value
-  );
+  // Seleciona a Quantidade de Dados
+  let quantidade_de_dados = document.querySelector(
+    ".dice-quantity-input"
+  ).value;
 
-  let dadoSelecionado = document.querySelector(".dice-type-selector").value;
+  // Seleciona o tipo de Dado
+  let dado_selecionado = document.querySelector(".dice-type-selector").value;
 
-  dadoSelecionado =
-    dadoSelecionado == "d2"
+  // Converte o dado Selecionado em um valor para ser usado no cálculo do giro
+  dado_selecionado =
+    dado_selecionado == "d2"
       ? 2
-      : dadoSelecionado == "d4"
+      : dado_selecionado == "d4"
       ? 4
-      : dadoSelecionado == "d6"
+      : dado_selecionado == "d6"
       ? 6
-      : dadoSelecionado == "d8"
+      : dado_selecionado == "d8"
       ? 8
-      : dadoSelecionado == "d10"
+      : dado_selecionado == "d10"
       ? 10
-      : dadoSelecionado == "d12"
+      : dado_selecionado == "d12"
       ? 12
       : 20;
 
-  for (let i = 0; i < quantidadeDeDados; i++) {
-    let resultado = Math.floor(Math.random() * dadoSelecionado) + 1;
-    window.alert(resultado);
-  }
+  // Seleção do Container onde ficarão os registros de rolagem
+  let container_roll_log = document.querySelector("#roll-log");
 
-  window.alert(resultado);
+  // Faz o loop com as rolagens de dado
+  for (let loop = 0; loop < quantidade_de_dados; loop++) {
+    // Cálculo da Rolagem
+    let resultado = Math.floor(Math.random() * dado_selecionado) + 1;
+
+    // Criação do Container que vai armazenar a Rolagem
+    let container_rolagens = document.createElement("div");
+    container_rolagens.className = "roll-result";
+
+    // Criação do Elemento de Texto onde nossas Rolagens vão aparecer
+    let text_roll = document.createElement("p");
+
+    // Atribuição do Resultado ao Elemento de Texto
+    text_roll.textContent = `Resultado do D${dado_selecionado}: ${resultado}`;
+
+    // Adiciona o texto ao container da rolagem
+    container_rolagens.appendChild(text_roll);
+
+    // Adiciona o container ao log de rolagens
+    container_roll_log.appendChild(container_rolagens);
+  }
 }
