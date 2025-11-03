b8fa36bcb39fb6481e33147ef81009a448b3ac5e
BaielsTosta <nunestosta@gmail.com>
2025-11-03 10:16:48 -0300

    Função rollDices() funcional com mais de 1 dado

--- public/js/app.js
+++ public/js/app.js
@@ -64,71 +64,14 @@ function addDices() {
   buttonRemover();
 }
 
-// Função para rolar os Dados
 function rollDices() {
-  // Seleciona a Quantidade de Dados
-  let quantidade_de_dados = document.querySelector(
-    ".dice-quantity-input"
-  ).value;
+  let fieldsets = document.querySelectorAll(".dice-information");
 
-  // Seleciona o tipo de Dado
-  let dado_selecionado = document.querySelector(".dice-type-selector").value;
-
-  // Converte o dado Selecionado em um valor para ser usado no cálculo do giro
-  dado_selecionado =
-    dado_selecionado == "d2"
-      ? 2
-      : dado_selecionado == "d4"
-      ? 4
-      : dado_selecionado == "d6"
-      ? 6
-      : dado_selecionado == "d8"
-      ? 8
-      : dado_selecionado == "d10"
-      ? 10
-      : dado_selecionado == "d12"
-      ? 12
-      : 20;
-
-  function singleRolls() {
-    let roll_saver = [];
-    // Faz o loop com as rolagens de dado
-    for (let loop = 0; loop < quantidade_de_dados; loop++) {
-      // Cálculo da Rolagem
-      let resultado = Math.floor(Math.random() * dado_selecionado) + 1;
-      roll_saver.push(resultado);
-    }
-    return roll_saver;
-  }
-
-  // Seleção do Container onde ficarão os registros de rolagem
-  let container_roll_log = document.querySelector("#roll-log");
-
-  // Criação do Container que vai armazenar a Rolagem
-  let container_rolagens = document.createElement("div");
-  container_rolagens.className = "roll-result";
-
-  // Criação do Elemento de Texto onde nossas Rolagens vão aparecer
-  let text_roll = document.createElement("p");
-  text_roll.textContent = `Resultados das Rolagens do D${dado_selecionado}: [${singleRolls()}]`;
-
-  // Adiciona o texto ao container da rolagem
-  container_rolagens.appendChild(text_roll);
-
-  // Adiciona o container ao log de rolagens
-  container_roll_log.appendChild(container_rolagens);
-}
-
-function rollDices2() {
-  let fieldsets = document.querySelectorAll(".dice-selection");
-  let quantities = document.querySelectorAll(".dice-quantity-input");
-  let dices_type = document.querySelectorAll(".dice-type-selector");
-  let quantidade_de_dados;
-  let dado_selecionado;
-
+  window.alert(fieldsets.length);
   fieldsets.forEach((fieldset, index) => {
     quantidade_de_dados = fieldset.querySelector(".dice-quantity-input").value;
     dado_selecionado = fieldset.querySelector(".dice-type-selector").value;
+
     // Converte o dado Selecionado em um valor para ser usado no cálculo do giro
     dado_selecionado =
       dado_selecionado == "d2"
@@ -144,6 +87,7 @@ function rollDices2() {
         : dado_selecionado == "d12"
         ? 12
         : 20;
+
     function singleRolls() {
       let roll_saver = [];
       // Faz o loop com as rolagens de dado
@@ -154,6 +98,7 @@ function rollDices2() {
       }
       return roll_saver;
     }
+
     // Seleção do Container onde ficarão os registros de rolagem
     let container_roll_log = document.querySelector("#roll-log");
 
@@ -163,7 +108,10 @@ function rollDices2() {
 
     // Criação do Elemento de Texto onde nossas Rolagens vão aparecer
     let text_roll = document.createElement("p");
-    text_roll.textContent = `Resultados das Rolagens do D${dado_selecionado}: [${singleRolls()}]`;
+    const resultadoDosDados = singleRolls();
+    text_roll.textContent = `${quantidade_de_dados}D${dado_selecionado} → [${resultadoDosDados.join(
+      ", "
+    )}]`;
 
     // Adiciona o texto ao container da rolagem
     container_rolagens.appendChild(text_roll);
